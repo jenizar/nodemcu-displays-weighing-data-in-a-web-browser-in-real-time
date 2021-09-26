cecd3c3c0e14fd2a5c80f1fc9226c29d3a987b88
John Eswin Nizar <jenizar@users.noreply.github.com>
2021-09-26 16:41:14 +0700

    php program
    php program

---
+++ display.php
@@ -0,0 +1,18 @@
+<?php
+  //koneksi ke database
+  $konek = mysqli_connect("localhost", "root", "", "websensor");
+
+  //baca isi table loadcell
+  $sql = mysqli_query($konek, "SELECT * FROM loadcell");
+  $data = mysqli_fetch_array($sql);
+  $nilai = $data["weight"];
+
+  if ($nilai < 1000)
+  {  
+   echo $nilai." Gr";
+  }
+  else {
+   $nilai = round($nilai/1000, 2); 
+   echo $nilai." Kg";
+  }
+ ?>
